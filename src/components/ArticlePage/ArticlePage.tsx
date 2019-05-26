0ac457cb0a191a0a5596cae1f17b847ee841d95e
Vitaly Harisov <vitaly@yandex-team.ru>
2019-05-26 11:55:36 +0300

    BEM filestructure for components

--- src/components/ArticlePage/ArticlePage.tsx
+++ src/components/ArticlePage/ArticlePage.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../Layout/layout"
 
 const ArticlePage = (data) => (
   <Layout pageContext={data.pageContext}>
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,9 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import { Nav } from './Nav'
-import "./layout.css"
+import Header from "../Header/Header"
+import { Nav } from '../Nav/Nav'
+import "./Layout.css"
 
 const Layout = ({ pageContext, children }) => (
   <StaticQuery
@@ -35,9 +34,5 @@ const Layout = ({ pageContext, children }) => (
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Layout
